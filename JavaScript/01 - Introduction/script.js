7bfd2bd02cf141090581177e0e04af10a569c505
georgesroberto <georgesroberto21@gmail>
2024-02-21 00:45:13 +0300

    Updated notes

---
+++ JavaScript/01 - Introduction/script.js
@@ -0,0 +1,73 @@
+//This is a comment
+
+console.log("Hooray! Here is my first JavaScript program");
+
+// alert("This is my external Javascript file");
+
+// DATA TYPES
+
+/**
+ * String -> "" ``
+ * Number -> int, float
+ * Boolean -> true, false
+ * Object
+ * undefined
+ * Null
+ */
+
+let name = "Georges isn't lazy";
+name = `Georges isn't lazy`;
+
+let age = 10;
+let weight = 10.5;
+
+let isMale = true;
+
+let Person = { tutorName: "Georges", tutorProfession: `Software Engineer` };
+
+let myVariable;
+
+let marks = null;
+
+// VARIABLES ;
+
+var myNewVariable = 120; //function-scoped
+
+let bestWay = "Variables";
+
+const idNumber = 129089;
+
+console.log(idNumber);
+
+/**
+ *1. camelCase -> oneTwoThreeFour
+ *
+ * PascalCase -> OneTwoThreeFour
+ * snake_case -> one_two_three_four
+ *
+ *
+ *2. Give descriptive terms for variable names
+ *
+ *3. variable names should begin with letters or $, _
+
+ */
+
+let $money = 10000;
+
+let m = 10000;
+
+let M = 5000;
+
+console.log("m: " + m);
+
+console.log("M:" + M);
+
+let _username = "UserName";
+
+
+
+// OPERATORS (=+/-*%)
+
+// CONTROL Structures (if, else if, switch, for, while)
+
+// FUNCTIONS function(){} 
