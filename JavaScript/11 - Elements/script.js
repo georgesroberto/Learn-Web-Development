a9692af6182c69aa9aa22d6ee34d6d2466d1a3be
Georges <georgesroberto21@gmail.com>
2024-03-05 21:58:30 +0300

    Elements

--- JavaScript/11 - Elements/script.js
+++ JavaScript/11 - Elements/script.js
@@ -1,59 +1,28 @@
-var imgEl = document.getElementById("image1");
+// CHANGING ATTR
 
-// imgEl.src = "image.png";
+let img = document.getElementById("image");
+img.src = "image.png";
+img.style.width = "100%";
+img.style.height = "auto";
 
-let homEl = document.getElementById("home");
+let p = document.getElementsByTagName("p");
+p[0].innerText = "This is our first paragraph";
+p[0].style.color = "tomato";
+p[0].style.fontSize = "24px";
 
-// homEl.removeChild(imgEl);
+// element.cloneNode() -->
+// document.createElement()
 
-// imgEl.parentNode.removeChild(imgEl);
+let button = document.createElement("button");
+let container = document.getElementById("container");
+container.appendChild(button);
 
-// CREATE NEW ELS
+let buttonText = document.createTextNode("ADD ITEM");
+button.appendChild(buttonText);
 
-var newP = document.createTextNode("Here is my new text");
+// document.removeElement()
+let li = document.getElementsByClassName("remove");
+li = li[0];
 
-let main = document.getElementById("main");
-
-let contactEl = document.createElement("div");
-
-main.appendChild(contactEl);
-
-contactEl.innerHTML = "My new div";
-
-contactEl.style.backgroundColor = "red";
-contactEl.style.padding = "10px";
-contactEl.style.marginTop = "20px";
-contactEl.style.marginBottom = "20px";
-
-// REPLACING ELEMENTS
-
-let img2 = document.createElement("img");
-img2.src = "image.png";
-img2.style.width = "100%";
-
-main.replaceChild(img2, contactEl);
-
-// END REPLACING ELEMNTS
-
-let aboutDiv = document.createElement("div");
-main.appendChild(aboutDiv);
-aboutDiv.style.backgroundColor = "rgb(15, 4, 232)";
-aboutDiv.style.backgroundColor = "azure";
-aboutDiv.style.marginTop = "20px";
-aboutDiv.style.marginBottom = "20px";
-aboutDiv.style.padding = "20px";
-
-let h2 = document.createElement("h2");
-h2.innerHTML = "About us";
-aboutDiv.appendChild(h2);
-
-let h3 = document.createElement("h3");
-h3.innerHTML = "What is Lorem Ipsum?";
-h3.style.color = "red";
-aboutDiv.appendChild(h3);
-
-let p = document.createTextNode(
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-);
-
-aboutDiv.appendChild(p);
+let ul = document.getElementById("list");
+ul.removeChild(li);
