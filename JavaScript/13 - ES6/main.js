46fab71b1446095ee4147d8f8bf0cc782e4cc4aa
Georges <georgesroberto21@gmail.com>
2024-03-06 21:50:19 +0300

    ES6

---
+++ JavaScript/13 - ES6/main.js
@@ -0,0 +1,19 @@
+// ECMASCRIPT Codes
+
+let numbers = [1, 2, 3, 4, 5];
+for (let index = 0; index < numbers.length; index++) {
+  console.log(numbers[index]);
+}
+
+for (let el of numbers) {
+  console.log(el);
+}
+
+let courses = { one: "HTML", two: "CSS", three: "JavaSxript" };
+
+for (let v in courses) {
+  console.log(v);
+}
+
+// for ... of
+// for ... in
