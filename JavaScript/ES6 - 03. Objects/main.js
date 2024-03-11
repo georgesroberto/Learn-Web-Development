0df699ccf16e8eaf245f77e259c08d65656b1eb2
Georges <georgesroberto21@gmail.com>
2024-03-11 23:40:55 +0300

    Updating Codes

--- JavaScript/ES6 - 03. Objects/main.js
+++ JavaScript/ES6 - 03. Objects/main.js
@@ -64,15 +64,56 @@ let instructors = class Tutors {
 let tutor1 = new instructor("Georges", 10);
 let tutor2 = new instructors("Dan", 10);
 
-console.log(tutor1.name);
-console.log(tutor2.name);
+// console.log(tutor1.name);
+// console.log(tutor2.name);
 
 let std1 = new Programmer("Philip", 12);
 let std2 = new Programmer("Brian", 12);
 let std3 = new Programmer("Clinton", 12, 12.5);
 
-console.log(std3.weight);
-console.log(Programmer.obesity(10, 12));
-console.log(std3.yob());
+// console.log(std3.weight);
+// console.log(Programmer.obesity(10, 12));
+// console.log(std3.yob());
 
-// INHERITANCE, OVERLOADING, OVERRIDING
+// INHERITANCE,  OVERRIDING
+class Person {
+  constructor(name, age, status = true) {
+    this.name = name;
+    this.age = age;
+    this.status = status;
+  }
+
+  bornYear() {
+    return `Born in ${2024 - this.age}`;
+  }
+  greet() {
+    return `Hello ${this.name}`;
+  }
+}
+
+let Anne = new Person("Antoinnette", 9);
+
+console.log(Anne.greet());
+
+class User extends Person {
+  constructor(name, age, status = false) {
+    super(); // Call the superclass constructor
+    this.name = name;
+    this.age = age;
+    this.status = status;
+  }
+
+  yearOfBirth() {
+    return `${this.name} was born in ${2024 - this.age}`;
+  }
+
+  greet() {
+    return `Welcome ${this.name}`;
+  }
+}
+
+const Georges = new User("Georges", 10);
+console.log(Georges.status);
+console.log(Georges.greet());
+console.log(Georges.bornYear()); // Output: "Born in 2014."
+console.log(Georges.yearOfBirth()); // Output: "Georges was born
